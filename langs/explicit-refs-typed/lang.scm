381c0777d10dbbc7abed6d77e48aa517e9b4816d
austin <austin.cawley@gmail.com>
2017-04-13 19:32:55 -0400

    Added trees to lang
    Signed-off-by: austin <austin.cawley@gmail.com>

--- langs/explicit-refs-typed/lang.scm
+++ langs/explicit-refs-typed/lang.scm
@@ -1,211 +1,252 @@
-(module lang (lib "eopl.ss" "eopl")                
+(module lang (lib "eopl.ss" "eopl")
 
-  ;; language for EXPLICIT-REFS
-  
-  (require "drscheme-init.scm")
-  
-  (provide (all-defined-out))
+        ;; language for EXPLICIT-REFS
 
-  ;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
-  
-  (define the-lexical-spec
-    '((whitespace (whitespace) skip)
-      (comment ("%" (arbno (not #\newline))) skip)
-      (identifier
-       (letter (arbno (or letter digit "_" "-" "?")))
-       symbol)
-      (number (digit (arbno digit)) number)
-      (number ("-" digit (arbno digit)) number)
-      ))
-  
-  (define the-grammar
-    '((program (expression) a-program)
+        (require "drscheme-init.scm")
+
+        (provide (all-defined-out))
+
+        ;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+
+        (define the-lexical-spec
+            '((whitespace (whitespace) skip)
+              (comment ("%" (arbno (not #\newline))) skip)
+              (identifier
+                  (letter (arbno (or letter digit "_" "-" "?")))
+                  symbol)
+              (number (digit (arbno digit)) number)
+              (number ("-" digit (arbno digit)) number)
+            ))
+
+        (define the-grammar
+            '((program (expression) a-program)
 
 
-      (expression
-       ("showstore")
-       showstore-exp)
+              (expression
+                  ("showstore")
+                  showstore-exp)
 
-      (expression (number) const-exp)
-      (expression
-       ("-" "(" expression "," expression ")")
-       diff-exp)
-      
-      (expression
-       ("zero?" "(" expression ")")
-       zero?-exp)
+              (expression (number) const-exp)
+              (expression
+                  ("-" "(" expression "," expression ")")
+                  diff-exp)
 
-      (expression
-       ("if" expression "then" expression "else" expression)
-       if-exp)
+              (expression
+                  ("zero?" "(" expression ")")
+                  zero?-exp)
 
-      (expression (identifier) var-exp)
+              (expression
+                  ("if" expression "then" expression "else" expression)
+                  if-exp)
 
-      (expression
-       ("let" identifier "=" expression "in" expression)
-       let-exp)   
+              (expression (identifier) var-exp)
 
-      (expression
-       ("proc" "(" identifier ":" type ")" expression)
-       proc-exp)
+              (expression
+                  ("let" identifier "=" expression "in" expression)
+                  let-exp)
 
-      (expression
-       ("(" expression expression ")")
-       call-exp)
+              (expression
+                  ("proc" "(" identifier ":" type ")" expression)
+                  proc-exp)
 
-      (expression
-       ("letrec"
-        type identifier "(" identifier ":" type ")" "=" expression
-        "in" expression)
-       letrec-exp)
+              (expression
+                  ("(" expression expression ")")
+                  call-exp)
 
-      
-      ;; new for explicit-refs
+              (expression
+                  ("letrec"
+                   type identifier "(" identifier ":" type ")" "=" expression
+                   "in" expression)
+                  letrec-exp)
 
-      (expression
-       ("begin" expression (arbno ";" expression) "end")
-       begin-exp)
 
-      (expression
-       ("newref" "(" expression ")")
-       newref-exp)
+              ;; new for explicit-refs
 
-      (expression
-       ("deref" "(" expression ")")
-       deref-exp)
+              (expression
+                  ("begin" expression (arbno ";" expression) "end")
+                  begin-exp)
 
-      (expression
-       ("setref" "(" expression "," expression ")")
-       setref-exp)
+              (expression
+                  ("newref" "(" expression ")")
+                  newref-exp)
 
-      (expression
-       ("for" identifier "=" expression "to" expression "(" expression ")")
-       for-exp)
+              (expression
+                  ("deref" "(" expression ")")
+                  deref-exp)
 
-      ;; New For Units
-      (expression
-          ("unitE")
-          unit-exp)
+              (expression
+                  ("setref" "(" expression "," expression ")")
+                  setref-exp)
 
-      ;; New For pairs
-      (expression
-          ("pair" "(" expression "," expression ")")
-          pair-exp)
+              (expression
+                  ("for" identifier "=" expression "to" expression "(" expression ")")
+                  for-exp)
 
-      (expression
-          ("unpair" "(" identifier "," identifier ")" "=" expression "in" expression)
-          unpair-exp)
+              ;; New For Units
+              (expression
+                  ("unitE")
+                  unit-exp)
 
-      ;; New for lists
-      (expression
-          ("emptylist" type)
-          emptylist-exp)
+              ;; New For pairs
+              (expression
+                  ("pair" "(" expression "," expression ")")
+                  pair-exp)
 
-      (expression
-          ("cons" "(" expression "," expression ")")
-          cons-exp)
+              (expression
+                  ("unpair" "(" identifier "," identifier ")" "=" expression "in" expression)
+                  unpair-exp)
 
-      (expression
-          ("null?" "(" expression ")")
-          null?-exp)
+              ;; New for lists
+              (expression
+                  ("emptylist" type)
+                  emptylist-exp)
 
-      (expression
-          ("car" "(" expression ")")
-          car-exp)
+              (expression
+                  ("cons" "(" expression "," expression ")")
+                  cons-exp)
 
-      (expression
-          ("cdr" "(" expression ")")
-          cdr-exp)
+              (expression
+                  ("null?" "(" expression ")")
+                  null?-exp)
 
-      ;; Types
-      (type
-       ("int")
-       int-type)
-      
-      (type
-       ("bool")
-       bool-type)
+              (expression
+                  ("car" "(" expression ")")
+                  car-exp)
 
-      (type
-       ("(" type "->" type ")")
-       proc-type)
+              (expression
+                  ("cdr" "(" expression ")")
+                  cdr-exp)
 
-      ;; Refs
-      (type
-          ("unit")
-          unit-type)
+              ;; New for Trees
 
-      (type
-          ("ref" "(" type ")")
-          ref-type)
+              (expression
+                  ("emptytree" type)
+                  emptrytree-exp)
 
-      ;; Pairs
-      (type
-          ("<" type "*" type ">")
-          pair-type)
+              (expression
+                  ("node" "(" expression "," expression "," expression ")")
+                  node-exp)
 
-      ;; Lists
-      (type
-          ("list" "(" type ")")
-          list-type)
-      ))
+              (expression
+                  ("nullT?" "(" expression ")")
+                  nullT?-exp)
 
-  ;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
-  
-  (sllgen:make-define-datatypes the-lexical-spec the-grammar)
-  
-  (define show-the-datatypes
-    (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
-  
-  (define scan&parse
-    (sllgen:make-string-parser the-lexical-spec the-grammar))
-  
-  (define just-scan
-    (sllgen:make-string-scanner the-lexical-spec the-grammar))
+              (expression
+                  ("getData" "(" expression ")")
+                  getData-exp)
 
-  ;;;;;;;;;;;;;;;; type-to-external-form ;;;;;;;;;;;;;;;;
+              (expression
+                  ("getLST" "(" expression ")")
+                  getLST-exp)
 
-  ;; type-to-external-form : Type -> List
-  ;; Page: 243
-  (define type-to-external-form
-    (lambda (ty)
-      (cases type ty
-             (int-type () "int")
-             (bool-type () "bool")
-             (unit-type () "unit")
-             (proc-type (arg-type result-type)
-                        (list->string (list
-                                          (type-to-external-form arg-type)
-                                          '- >
-                                          (type-to-external-form result-type))))
+              (expression
+                  ("getRST" "(" expression ")")
+                  getRST-exp)
 
-             ;; Refs
-             (ref-type (arg-type)
-                       (string-append
-                           "ref" "("
-                           (type-to-external-form arg-type)
-                           ")"
-                           ))
+              ;; Types
+              (type
+                  ("int")
+                  int-type)
 
-             ;; Pairs
-             (pair-type (typeA typeB)
-                        (string-append
-                            "<"
-                            (type-to-external-form typeA)
-                            "*"
-                            (type-to-external-form typeB)
-                            ">"
-                            ))
+              (type
+                  ("bool")
+                  bool-type)
 
-             ;; Lists
-             (list-type (ltype)
-                        (string-append
-                            "list" "("
-                            (type-to-external-form ltype)
-                            ")"
-                            ))
+              (type
+                  ("(" type "->" type ")")
+                  proc-type)
 
-             )
-      ))
-  
-  )
+              ;; Refs
+              (type
+                  ("unit")
+                  unit-type)
+
+              (type
+                  ("ref" "(" type ")")
+                  ref-type)
+
+              ;; Pairs
+              (type
+                  ("<" type "*" type ">")
+                  pair-type)
+
+              ;; Lists
+              (type
+                  ("list" "(" type ")")
+                  list-type)
+
+              ;; Trees
+              (type
+                  ("tree" "(" type ")")
+                  tree-type)
+            ))
+
+
+
+        ;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+
+        (sllgen:make-define-datatypes the-lexical-spec the-grammar)
+
+        (define show-the-datatypes
+            (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
+        (define scan&parse
+            (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+        (define just-scan
+            (sllgen:make-string-scanner the-lexical-spec the-grammar))
+
+        ;;;;;;;;;;;;;;;; type-to-external-form ;;;;;;;;;;;;;;;;
+
+        ;; type-to-external-form : Type -> List
+        ;; Page: 243
+        (define type-to-external-form
+            (lambda (ty)
+                (cases type ty
+                       (int-type () "int")
+                       (bool-type () "bool")
+                       (unit-type () "unit")
+                       (proc-type (arg-type result-type)
+                                  (list->string (list
+                                                    (type-to-external-form arg-type)
+                                                    '- >
+                                                    (type-to-external-form result-type))))
+
+                       ;; Refs
+                       (ref-type (arg-type)
+                                 (string-append
+                                     "ref" "("
+                                     (type-to-external-form arg-type)
+                                     ")"
+                                     ))
+
+                       ;; Pairs
+                       (pair-type (typeA typeB)
+                                  (string-append
+                                      "<"
+                                      (type-to-external-form typeA)
+                                      "*"
+                                      (type-to-external-form typeB)
+                                      ">"
+                                      ))
+
+                       ;; Lists
+                       (list-type (ltype)
+                                  (string-append
+                                      "list" "("
+                                      (type-to-external-form ltype)
+                                      ")"
+                                      ))
+
+                       ;; Trees
+                       (tree-type (tType)
+                                  (string-append
+                                      "tree" "("
+                                      (type-to-external-form tType)
+                                      ")"
+                                      ))
+
+                       )
+                ))
+
+        )
